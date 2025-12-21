e6261572efcf55759c97dc6a52782229b9bd4d38
Jason Li <56463342+jla690@users.noreply.github.com>
2025-12-20 23:16:33 -0800

    Features (#3)
    * changes
    
    * many more changes
    
    * use fragment instead

--- app/Projects.tsx
+++ app/Projects.tsx
@@ -7,23 +7,40 @@ const Projects = () => {
       <h1 className="text-2xl font-bold my-4 mx-10 text-center" id="projects">
         Projects
       </h1>
-      <div className="flex mx-10">
+      <div className="mx-10 grid grid-cols-3 gap-4 my-10">
         <Card
           isPublic={true}
           title="Unofficial AniList Clone"
           link="https://github.com/jla690/unofficial_anilist_program"
           description="Full-stack web app to retrieve and display real-time anime and manga data"
+          languages={["JavaScript", "Python"]}
         ></Card>
         <Card
           isPublic={true}
           link="https://github.com/jla690/experience-checker"
           title="LinkedIn Experience Checker"
           description="Chrome extension to summarize YOE data in LinkedIn job postings"
+          languages={["JavaScript"]}
         ></Card>
         <Card
           isPublic={false}
           title="Decaf Compiler (Not Public)"
           description="Fully functional compiler for the Decaf language (Java-like teaching language)"
+          languages={["C++", "Yacc", "Bison"]}
+        ></Card>
+        <Card
+          isPublic={true}
+          title="Home Defense System"
+          description="Project using Beaglebone Black to monitor home using a variety of different sensors"
+          link="https://github.com/jla690/Home_Defense_System"
+          languages={["C++", "C"]}
+        ></Card>
+        <Card
+          isPublic={true}
+          title="Item Manager"
+          description="Website that can be used for cataloguing items like a store would. Uses SQLite database"
+          link="https://github.com/jla690/flask-website-project"
+          languages={["Python"]}
         ></Card>
       </div>
     </div>
--- app/Skills.tsx
+++ app/Skills.tsx
@@ -2,18 +2,32 @@ import React from "react";
 
 const Skills = () => {
   return (
-    <div className="px-15 pb-15">
-      <h2 id="skills" className="font-bold text-xl">
+    <div className="px-15 pb-20 max-w-4xl mx-auto">
+      <h2 id="skills" className="font-bold text-2xl mb-6">
         Technical Skills
       </h2>
-      <div className="font-bold">Languages:</div>
-      <div>C++, C, Java, Python, TypeScript, JavaScript, SQL</div>
-      <div className="font-bold">Libraries/Frameworks: </div>
-      <div>AWS SDK, Axios, FastAPI, Flask, React, Tailwind CSS, Next.js</div>
-      <div className="font-bold">Testing Frameworks: </div>
-      <div className="">JUnit, Mockito, Jest, Mocha</div>
-      <div className="font-bold">Other Technologies: </div>
-      <div>Git, Perforce, Linux, Amazon RDS, MySQL, PostgreSQL, GraphQL</div>
+      <div className="space-y-3">
+        <div>
+          <span className="font-bold">Languages: </span>
+          <span>C++, C, Java, Python, TypeScript, JavaScript, SQL</span>
+        </div>
+        <div>
+          <span className="font-bold">Libraries/Frameworks: </span>
+          <span>
+            AWS SDK, Axios, FastAPI, Flask, React, Tailwind CSS, Next.js
+          </span>
+        </div>
+        <div>
+          <span className="font-bold">Testing Frameworks: </span>
+          <span>JUnit, Mockito, Jest, Mocha</span>
+        </div>
+        <div>
+          <span className="font-bold">Other Technologies: </span>
+          <span>
+            Git, Perforce, Linux, Amazon RDS, MySQL, PostgreSQL, GraphQL
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
